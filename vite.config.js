6b152a57fe3343b2ce0696487fd31972de5391e4
qscftyjm1234 <qscftyjm1234@gmail.com>
2023-07-20 13:49:05 +0800

    推部屬

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/magazine/',  // 部屬專案名稱
   plugins: [vue()],
   css: {
       postcss: {
